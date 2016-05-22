ad2a915493012647337c346c51d573efa8c698cd
Hari <hari.140489@gmail.com>
2016-05-22 09:38:43 +0530

    Added Docker file

--- pricing-service/src/main/java/com/globomart/PricingServiceApplication.java
+++ pricing-service/src/main/java/com/globomart/PricingServiceApplication.java
@@ -9,7 +9,6 @@ import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 
-@EnableCircuitBreaker
 @SpringBootApplication
 @EnableDiscoveryClient
 public class PricingServiceApplication {
