5d9fc8e83d9a96999dd7446f9af33c9b846f790a
Hari Krishna <hari.140489@gmail.com>
2016-05-23 09:06:16 -0500

    Removed unnecessary code

--- client-gateway/src/main/java/com/globomart/ClientGatewayApplication.java
+++ client-gateway/src/main/java/com/globomart/ClientGatewayApplication.java
@@ -1,40 +1,17 @@
 package com.globomart;
 
-import com.globomart.vo.ProductVo;
-import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
-import org.springframework.context.annotation.Bean;
-import org.springframework.core.ParameterizedTypeReference;
-import org.springframework.http.*;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
-import org.springframework.web.util.UriComponentsBuilder;
-
-import java.util.Collections;
-import java.util.List;
 
 @EnableCircuitBreaker
 @EnableZuulProxy
-@EnableDiscoveryClient
 @SpringBootApplication
 public class ClientGatewayApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ClientGatewayApplication.class, args);
-    }
-
-    @LoadBalanced
-    @Bean
-    public RestTemplate restTemplate() {
-        return new RestTemplate();
     }
 }
